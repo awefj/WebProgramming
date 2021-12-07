089332dffeb03384e8272d316daff3e877fe3163
Jung <yonggeon0284@gmail.com>
2021-12-07 09:53:41 +0900

    .

--- project/routes/page.js
+++ project/routes/page.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const { Post, User, Hashtag } = require('../models');
 
 const router = express.Router();
 
 router.use((req, res, next) => {
     res.locals.user = req.user;
+    res.locals.followerCount = req.user ? req.user.Followers.length : 0;
+    res.locals.followingCount = req.user ? req.user.Followings.length : 0;
+    res.locals.followerIdList = req.user ? req.user.Followings.map(f => f.id) : [];
     next();
-});
-
-router.get('/', (req, res) => {
-    res.render('main', { title: 'Web47 SNS' });
 });
 
 router.get('/profile', isLoggedIn, (req, res) => {
@@ -19,5 +19,15 @@ router.get('/profile', isLoggedIn, (req, res) => {
 router.get('/account', isNotLoggedIn, (req, res) => {
     res.render('account', { title: 'Web47 SNS - 회원가입' });
 })
+
+router.get('/', (req, res) => {
+    res.render('main', { title: 'Web47 SNS' });
+});
+
+router.use((req, res, next) => {
+    res.locals.user = req.user;
+    next();
+});
+
 
 module.exports = router;
