699508e04d18fecf003cdf729bf4cd08fb74b89e
Jung <yonggeon0284@gmail.com>
2021-12-10 22:10:07 +0900

    follow works

--- project/routes/page.js
+++ project/routes/page.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { Op } = require('sequelize');
 const { isLoggedIn, isNotLoggedIn, isEmailConfirmed } = require('./middlewares');
 const { Post, User, Hashtag } = require('../models');
+const e = require('express');
 
 const router = express.Router();
 
@@ -13,16 +15,19 @@ router.use((req, res, next) => {
 });
 
 router.get('/home', isLoggedIn, isEmailConfirmed, async (req, res, next) => {
-    let pageNum = req.query.pageNum ? req.query.pageNum - 1 : 0;//요청한 페이지
+    const perPage = 9; //페이지 당 9개 포스트로 제한
+    const pageCount = Math.ceil(Post.count() / perPage); //전체 페이지 수
+    let pageNum = req.query.pageNum ? parseInt(req.query.pageNum) - 1 : 0;//요청한 페이지 - 특정 페이지 요청이 없으면 0
+    if (pageNum > pageCount) pageNum = pageCount; //요청 페이지가 존재하는 페이지보다 큰 경우 마지막 페이지로 설정
     try {
         const posts = await Post.findAll({
-            include: {//해당 페이지 9개만 가져와야됨
-                offset: 9 * pageNum,
-                limit: 9,
+            offset: perPage * pageNum,
+            limit: perPage,//해당 페이지 9개만 가져와야됨
+            include: {
                 model: User,
                 attributes: ['id', 'name'],
             },
-            order: [['createdAt', "DESC"]],
+            order: [['createdAt', 'DESC']],
         });
         res.render('home', {
             title: '메인 - Web47 SNS',
@@ -34,13 +39,25 @@ router.get('/home', isLoggedIn, isEmailConfirmed, async (req, res, next) => {
     }
 });
 
-router.get('/new', isLoggedIn, isEmailConfirmed, (req,res)=>{
-    res.render('new', {title: '포스트 작성 - Web47 SNS'});
-})
+router.get('/new', isLoggedIn, isEmailConfirmed, (req, res) => {
+    res.render('new', { title: '포스트 작성 - Web47 SNS' });
+});
 
-router.get('/follow', isLoggedIn, isEmailConfirmed, (req, res)=>{
-    res.render('follow', {title: '팔로우 관리 - Web47 SNS'});
-})
+router.get('/follow', isLoggedIn, isEmailConfirmed, async (req, res, next) => {
+    const selfID = req.user.id;
+    let following = req.user.Followings.map(f => f.id);
+    following = following.push(selfID);
+    console.log("following : " + following);
+    const followingAttr = {};//출처 : https://velog.io/@jujube0/Sequelize-%EB%AC%B8%EC%A0%9C%ED%95%B4%EA%B2%B0
+    if (following) { followingAttr[Op.not] = following; } else { followingAttr[Op.not] = selfID; }
+    const notFollowing = await User.findAll({
+        attributes: ['id', 'name'],
+        where: { id: followingAttr },
+        order: [['createdAt', 'DESC']],
+    });
+    console.log("not following : " + notFollowing);
+    res.render('follow', { title: '팔로우 관리 - Web47 SNS', notFollow: notFollowing });
+});
 
 router.get('/profile', isLoggedIn, (req, res) => {
     res.render('profile', { title: '내 정보 - Web47 SNS', currentTime: `${Date()}` });
--- project/routes/user.js
+++ project/routes/user.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { isLoggedIn } = require('./middlewares');
+const { isLoggedIn, isEmailConfirmed } = require('./middlewares');
 const User = require('../models/user');
 const router = express.Router();
 
-router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
+router.post('/:id/follow', isLoggedIn, isEmailConfirmed, async (req, res, next) => {
     try {
         const user = await User.findOne({ where: { id: req.user.id } });
         if (user) {
@@ -18,11 +18,11 @@ router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
     }
 });
 
-router.post('/:id/unfollow', isLoggedIn, async (req, res, next) => {
+router.post('/:id/unfollow', isLoggedIn, isEmailConfirmed, async (req, res, next) => {
     try {
         const user = await User.findOne({ where: { id: req.user.id } });
         if (user) {
-            await user.destroyFollowing(parseInt(req.params.id, 10));
+            await user.removeFollowing(parseInt(req.params.id, 10));
             res.send('success');
         } else {
             res.status(404).send('no user');
