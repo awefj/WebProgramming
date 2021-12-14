d052c15eac4e729f8473ac6064994d518f94c27b
Jung <yonggeon0284@gmail.com>
2021-12-14 23:21:07 +0900

    .

--- project/models/post.js
+++ project/models/post.js
@@ -36,6 +36,6 @@ module.exports = class Post extends Sequelize.Model {
     }
     static associate(db) {
         db.Post.belongsTo(db.User);
-        db.Post.belongsToMany(db.Hashtag, { through: 'PostHashtag' });
+        db.Post.belongsToMany(db.Hashtag, { through: 'posthashtag' });
     }
 };
